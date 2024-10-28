e86bda2bee9af6986bfac154828101f5de88d71e
Angel Reyna <week.dreams.lol@gmail.com>
2024-10-27 18:40:47 -0600

    cambio de pc

--- api/schemas/order.schema.js
+++ api/schemas/order.schema.js
@@ -1,7 +1,10 @@
-const Joi = require('joi')
+import Joi from 'joi'
 
 const id = Joi.number().integer()
 const customerId = Joi.number().integer()
+const orderId = Joi.number().integer()
+const productId = Joi.number().integer()
+const amount = Joi.number().integer().min(1)
 
 const getOrderSchema = Joi.object({
   id: id.required()
@@ -11,7 +14,10 @@ const createOrderSchema = Joi.object({
   customerId: customerId.required()
 })
 
-module.exports = {
-  getOrderSchema,
-  createOrderSchema
-}
+const addItemSchema = Joi.object({
+  orderId: orderId.required(),
+  productId: productId.required(),
+  amount: amount.required()
+})
+
+export { getOrderSchema, createOrderSchema, addItemSchema }
--- api/services/order.services.js
+++ api/services/order.services.js
@@ -6,6 +6,11 @@ class OrderService {
     return newOrder
   }
 
+  async addItem (data) {
+    const newItem = await models.OrderProduct.create(data)
+    return newItem
+  }
+
   async find () {
     return []
   }
@@ -16,7 +21,8 @@ class OrderService {
         {
           association: 'customer',
           include: ['user']
-        }
+        },
+        'items'
       ]
     })
     return order
